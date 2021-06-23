a17d6ac7e30a04742c8a1c1df5b31db0456fb689
Dasharadhan <das.mahalingam@gmail.com>
2021-06-23 14:59:39 -0400

    Added test Added test for checking KinematicsSolver::loadManipulator()

--- test/kinlib_test.cpp
+++ test/kinlib_test.cpp
@@ -1,23 +1,35 @@
-#include <ros/ros.h>
 #include <kinlib/kinlib_kinematics.h>
 #include <kinlib_test/config.h>
-#include <gtest/gtest.h>
 #include <boost/filesystem/path.hpp>
 #include <armadillo>
 #include <fstream>
+#include <gtest/gtest.h>
 
 class KinlibTest : public testing::Test
 {
+  protected:
+    void SetUp() override
+    {
+      boost::filesystem::path res_dir(KINLIB_TEST_RESOURCES_DIR);
+      
+      urdf_file_path_ = (res_dir / "baxter.urdf").string();
+      jnt_ang_file_path_ = (res_dir / "JointAngles.csv").string();
+    }
 
+    kinlib::KinematicsSolver kin_solver_;
+    std::string urdf_file_path_;
+    std::string jnt_ang_file_path_;
+};
+
+TEST_F(KinlibTest, KinSolverLoadManipulator)
+{
+  EXPECT_EQ(
+      kin_solver_.loadManipulator(urdf_file_path_, "base", "left_gripper_base"),
+      true);
 }
 
 int main(int argc, char** argv)
 {
-  ros::init(argc, argv, "knilib_test");
-
-  ros::NodeHandle nh;
-
-  ROS_INFO("Node Starting!");
-
-  arma::
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
 }
